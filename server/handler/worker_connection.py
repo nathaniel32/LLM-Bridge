84c2df2fb18a74f5be7a774462833762650d9a7f
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-08 23:48:03 +0100

    update

--- server/handler/worker_connection.py
+++ server/handler/worker_connection.py
@@ -3,6 +3,7 @@ from fastapi import WebSocket, WebSocketDisconnect
 from common.models import WorkerServerActionType, StatusType
 import logging
 import json
+from server.utils import ws_response
 if TYPE_CHECKING:
     from server.handler.connection_manager import ConnectionManager
 
@@ -10,6 +11,9 @@ class WorkerConnection:
     def __init__(self, connection_manager:"ConnectionManager", connection: WebSocket):
         self.connection_manager = connection_manager
         self.connection = connection
+
+    async def send(self, action, content=None):
+        await ws_response(websockets=[self.connection], action=action, content=content)
 
     async def _message_listener(self):
         try:
