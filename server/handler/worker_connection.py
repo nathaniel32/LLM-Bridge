d3dbafecaa2e5376700b4e89870722c2ea42ce76
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-08 14:54:39 +0100

    update

--- server/handler/worker_connection.py
+++ server/handler/worker_connection.py
@@ -1,4 +1,8 @@
 from typing import TYPE_CHECKING
+from fastapi import WebSocket, WebSocketDisconnect
+from common.models import WorkerServerActionType, StatusType
+import logging
+import json
 if TYPE_CHECKING:
     from server.handler.connection_manager import ConnectionManager
 
@@ -6,5 +10,30 @@ class WorkerConnection:
     def __init__(self, connection_manager:"ConnectionManager"):
         self.connection_manager = connection_manager
 
+    async def _message_listener(self, websocket:WebSocket):
+        try:
+            while True:
+                message = await websocket.receive()
+
+                if message.get("text"):
+                    try:
+                        data = json.loads(message["text"])
+                        action = data.get("action")
+
+                        match action:
+                            case WorkerServerActionType.LOG:
+                                pass
+                            case _:
+                                await self.send(message="Unknown action", status_type=StatusType.ERROR)
+                    except Exception:
+                        await self.send(message=message.get("text"))
+
+        except WebSocketDisconnect:
+            logging.info("WebSocket disconnected")
+        except Exception as e:
+            logging.exception("Unexpected error in websocket listener")
+        finally:
+            logging.info("END!")
+
     async def bind(self):
-        pass
+        await self._message_listener()
