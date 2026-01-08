d580c555a1d2b48b8f711de86808b552d79eb6dd
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-08 23:56:26 +0100

    update

--- server/handler/worker_connection.py
+++ server/handler/worker_connection.py
@@ -1,9 +1,11 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 from fastapi import WebSocket, WebSocketDisconnect
 from common.models import WorkerServerActionType, StatusType
 import logging
 import json
+from server.handler.group_manager import GroupManager
 from server.utils import ws_response
+import asyncio
 if TYPE_CHECKING:
     from server.handler.connection_manager import ConnectionManager
 
@@ -11,9 +13,23 @@ class WorkerConnection:
     def __init__(self, connection_manager:"ConnectionManager", connection: WebSocket):
         self.connection_manager = connection_manager
         self.connection = connection
+        self.job_event: Optional[asyncio.Event] = None
+        self.group_manager = Optional[GroupManager] = None
 
     async def send(self, action, content=None):
         await ws_response(websockets=[self.connection], action=action, content=content)
+
+    async def prompt(self, group_manager:GroupManager):
+        if self.job_event is None:
+            try:
+                self.job_event = asyncio.Event()
+                self.group_manager = group_manager
+                self.group_manager.send("Sending Job to Worker...")
+            finally:
+                self.job_event = None
+                self.group_manager = None
+        else:
+            raise Exception("Worker busy, please try again later!")
 
     async def _message_listener(self):
         try:
