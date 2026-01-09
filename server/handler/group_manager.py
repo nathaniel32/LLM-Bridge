b7d5388dbe8ddedc2ecbcdfde4e17ad226d61a9f
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-09 16:12:48 +0100

    update

--- server/handler/group_manager.py
+++ server/handler/group_manager.py
@@ -31,6 +31,11 @@ class ChatContext(BaseModel):
 
     def finish_interaction(self):
         self.active_interaction = None
+
+    def edit_interaction(self, interaction_id, prompt):
+        self.active_interaction = next((i for i in self.interaction_history if i.id == interaction_id), None)
+        self.active_interaction.prompt = prompt
+        self.active_interaction.response = ""
     
     def get_chat_message(self):
         messages = []
