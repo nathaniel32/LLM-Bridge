4902fae15e717e248202ff40b95d929a5029d2aa
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-11 03:03:12 +0100

    update

--- server/handler/group_manager.py
+++ server/handler/group_manager.py
@@ -32,12 +32,12 @@ class GroupManager:
     async def remove_client(self, client_connection:"ClientConnection"):
         self.client_connections.remove(client_connection)
         client_connection.group_manager = None
-        await client_connection.send(message=MessageModel(text=f"Leave from {self.group_infos.id}", status=StatusType.WARNING), content=ClientContent(joined_group_infos=GroupInfos(id="", name="")))
+        await client_connection.send(message=MessageModel(text=f"Leave from {self.group_infos.id}", status=StatusType.WARNING), content=ClientContent(joined_group_infos=GroupInfos(id="", name=""))) # reset joined_group_infos
 
     async def delete_group(self):
         await self.connection_manager.remove_group_manager(self)
         await self.abort_interaction()
-        for client in self.client_connections:
+        for client in self.client_connections[:]: # copy
             await self.remove_client(client)
 
     async def send(self, message=None, content=None, action=None, client_connections=None):
