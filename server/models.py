0970d0b2f68b0406e79cc0e056ad2451cb0f2c6c
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-11 04:29:56 +0100

    update

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional
-from common.models import Interaction, InteractionStatus
+from common.models import Interaction
 import json
 
 class RequestError(Exception):
